8517ed32166f747beec32a4501128e580a33413a
mchien2002 <minhchien77777@gmail.com>
2023-01-01 15:23:00 +0700

    HORIZONTAL MOVEMENT

--- FreeRunner/Assets/Scripts/Player/PlayerMove.cs
+++ FreeRunner/Assets/Scripts/Player/PlayerMove.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     float moveSpeed = 3;
+    float leftRightSpeed = 4;
 
     // Start is called before the first frame update
     // void Start()
@@ -14,5 +15,15 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && this.gameObject.transform.position.x > LevelBoundary.leftSide)
+        {
+
+            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed, Space.World);
+        }
+        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))&& this.gameObject.transform.position.x < LevelBoundary.rightSide)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * leftRightSpeed, Space.World);
+        }
+
     }
 }
